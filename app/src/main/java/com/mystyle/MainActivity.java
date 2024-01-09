b6115a9eb6c17220e5313aea2b7a8ddcadf3e53e
Gohary Dev <66566545+goharydev@users.noreply.github.com>
2024-01-09 03:54:45 +0200

    Add files via upload

---
+++ app/src/main/java/com/mystyle/MainActivity.java
@@ -0,0 +1,329 @@
+package com.mystyle;
+
+import android.animation.*;
+import android.app.*;
+import android.app.AlertDialog;
+import android.content.*;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.os.Bundle;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+import java.io.*;
+import java.io.InputStream;
+import java.text.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private Timer _timer = new Timer();
+	private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+	
+	private Toolbar _toolbar;
+	private AppBarLayout _app_bar;
+	private CoordinatorLayout _coordinator;
+	
+	private ArrayList<HashMap<String, Object>> map1 = new ArrayList<>();
+	
+	private RequestNetwork wifi;
+	private RequestNetwork.RequestListener _wifi_request_listener;
+	private Intent start = new Intent();
+	private TimerTask check;
+	private DatabaseReference secure = _firebase.getReference("secure");
+	private ChildEventListener _secure_child_listener;
+	private Intent color_0x = new Intent();
+	private AlertDialog.Builder d;
+	private AlertDialog.Builder c;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		_app_bar = findViewById(R.id._app_bar);
+		_coordinator = findViewById(R.id._coordinator);
+		_toolbar = findViewById(R.id._toolbar);
+		setSupportActionBar(_toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
+		wifi = new RequestNetwork(this);
+		d = new AlertDialog.Builder(this);
+		c = new AlertDialog.Builder(this);
+		
+		_wifi_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2, HashMap<String, Object> _param3) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				final HashMap<String, Object> _responseHeaders = _param3;
+				secure.addListenerForSingleValueEvent(new ValueEventListener() {
+						@Override
+						public void onDataChange(DataSnapshot _dataSnapshot) {
+								map1 = new ArrayList<>();
+								try {
+										GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+										for (DataSnapshot _data : _dataSnapshot.getChildren()) {
+												HashMap<String, Object> _map = _data.getValue(_ind);
+												map1.add(_map);
+										}
+								}
+								catch (Exception _e) {
+										_e.printStackTrace();
+								}
+								if (map1.get((int)0).get("{G}").toString().equals("ok")) {
+										
+								}
+								else {
+										color_0x.setClass(getApplicationContext(), Olor_0xActivity.class);
+										startActivity(color_0x);
+								}
+						}
+						@Override
+						public void onCancelled(DatabaseError _databaseError) {
+						}
+				});
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				d.setTitle("لا يوجد اتصال بالانترنت !");
+				d.setIcon(R.drawable.tree);
+				d.setPositiveButton("اعد المحاولة", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						start.setClass(getApplicationContext(), MainActivity.class);
+						startActivity(start);
+					}
+				});
+				d.setNegativeButton("الخروج", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						finishAffinity();
+					}
+				});
+				d.setCancelable(false);
+				d.create().show();
+			}
+		};
+		
+		_secure_child_listener = new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildChanged(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildMoved(DataSnapshot _param1, String _param2) {
+				
+			}
+			
+			@Override
+			public void onChildRemoved(DataSnapshot _param1) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError _param1) {
+				final int _errorCode = _param1.getCode();
+				final String _errorMessage = _param1.getMessage();
+				
+			}
+		};
+		secure.addChildEventListener(_secure_child_listener);
+	}
+	
+	private void initializeLogic() {
+		wifi.startRequestNetwork(RequestNetworkController.GET, "https://google.com", "w", _wifi_request_listener);
+		if (SketchwareUtil.isConnected(getApplicationContext())) {
+			start.setClass(getApplicationContext(), SplashActivity.class);
+			startActivity(start);
+			check = new TimerTask() {
+				@Override
+				public void run() {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							secure.addListenerForSingleValueEvent(new ValueEventListener() {
+									@Override
+									public void onDataChange(DataSnapshot _dataSnapshot) {
+											map1 = new ArrayList<>();
+											try {
+													GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+													for (DataSnapshot _data : _dataSnapshot.getChildren()) {
+															HashMap<String, Object> _map = _data.getValue(_ind);
+															map1.add(_map);
+													}
+											}
+											catch (Exception _e) {
+													_e.printStackTrace();
+											}
+											if (map1.get((int)0).get("{G}").toString().equals("ok")) {
+													
+											}
+											else {
+													color_0x.setClass(getApplicationContext(), Olor_0xActivity.class);
+													startActivity(color_0x);
+											}
+									}
+									@Override
+									public void onCancelled(DatabaseError _databaseError) {
+									}
+							});
+						}
+					});
+				}
+			};
+			_timer.scheduleAtFixedRate(check, (int)(0), (int)(400));
+		}
+		else {
+			d.setTitle("لا يوجد اتصال بالانترنت !");
+			d.setIcon(R.drawable.tree);
+			d.setPositiveButton("اعد المحاولة", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface _dialog, int _which) {
+					start.setClass(getApplicationContext(), MainActivity.class);
+					startActivity(start);
+				}
+			});
+			d.setNegativeButton("الخروج", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface _dialog, int _which) {
+					finishAffinity();
+				}
+			});
+			d.setCancelable(false);
+			d.create().show();
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		c.setTitle("هل تريد الخروج");
+		c.setPositiveButton("نعم", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface _dialog, int _which) {
+				finishAffinity();
+			}
+		});
+		c.setNegativeButton("لا", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface _dialog, int _which) {
+				
+			}
+		});
+		c.create().show();
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
---
+++ app/src/main/java/com/mystyle/SplashActivity.java
@@ -0,0 +1,387 @@
+package com.mystyle;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.Typeface;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.os.Bundle;
+import android.text.*;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.io.*;
+import java.io.InputStream;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class SplashActivity extends AppCompatActivity {
+	
+	private Timer _timer = new Timer();
+	
+	private LinearLayout splash;
+	private LinearLayout start;
+	private LinearLayout login;
+	private LinearLayout linear3;
+	private ImageView imageview1;
+	private ImageView imageview3;
+	private LinearLayout linear4;
+	private LinearLayout linear8;
+	private LinearLayout linear5;
+	private ImageView imageview2;
+	private ScrollView vscroll1;
+	private LinearLayout linear6;
+	private TextView textview1;
+	private Button button1;
+	private LinearLayout linear11;
+	private LinearLayout linear13;
+	private ImageView imageview4;
+	private EditText email;
+	private EditText password;
+	private LinearLayout linear14;
+	private LinearLayout linear12;
+	private LinearLayout linear15;
+	private ImageView imageview5;
+	private TextView textview2;
+	private Button createuser;
+	private Button loginbtn;
+	
+	private TimerTask thjk;
+	private FirebaseAuth auth;
+	private OnCompleteListener<AuthResult> _auth_create_user_listener;
+	private OnCompleteListener<AuthResult> _auth_sign_in_listener;
+	private OnCompleteListener<Void> _auth_reset_password_listener;
+	private OnCompleteListener<Void> auth_updateEmailListener;
+	private OnCompleteListener<Void> auth_updatePasswordListener;
+	private OnCompleteListener<Void> auth_emailVerificationSentListener;
+	private OnCompleteListener<Void> auth_deleteUserListener;
+	private OnCompleteListener<Void> auth_updateProfileListener;
+	private OnCompleteListener<AuthResult> auth_phoneAuthListener;
+	private OnCompleteListener<AuthResult> auth_googleSignInListener;
+	
+	private Intent i = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.splash);
+		initialize(_savedInstanceState);
+		FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		splash = findViewById(R.id.splash);
+		start = findViewById(R.id.start);
+		login = findViewById(R.id.login);
+		linear3 = findViewById(R.id.linear3);
+		imageview1 = findViewById(R.id.imageview1);
+		imageview3 = findViewById(R.id.imageview3);
+		linear4 = findViewById(R.id.linear4);
+		linear8 = findViewById(R.id.linear8);
+		linear5 = findViewById(R.id.linear5);
+		imageview2 = findViewById(R.id.imageview2);
+		vscroll1 = findViewById(R.id.vscroll1);
+		linear6 = findViewById(R.id.linear6);
+		textview1 = findViewById(R.id.textview1);
+		button1 = findViewById(R.id.button1);
+		linear11 = findViewById(R.id.linear11);
+		linear13 = findViewById(R.id.linear13);
+		imageview4 = findViewById(R.id.imageview4);
+		email = findViewById(R.id.email);
+		password = findViewById(R.id.password);
+		linear14 = findViewById(R.id.linear14);
+		linear12 = findViewById(R.id.linear12);
+		linear15 = findViewById(R.id.linear15);
+		imageview5 = findViewById(R.id.imageview5);
+		textview2 = findViewById(R.id.textview2);
+		createuser = findViewById(R.id.createuser);
+		loginbtn = findViewById(R.id.loginbtn);
+		auth = FirebaseAuth.getInstance();
+		
+		login.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				start.setVisibility(View.GONE);
+			}
+		});
+		
+		password.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence _param1, int _param2, int _param3, int _param4) {
+				final String _charSeq = _param1.toString();
+				if (password.getText().toString().length() < 10) {
+					linear15.setVisibility(View.VISIBLE);
+				}
+				else {
+					linear15.setVisibility(View.GONE);
+				}
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence _param1, int _param2, int _param3, int _param4) {
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable _param1) {
+				
+			}
+		});
+		
+		createuser.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				if (email.getText().toString().equals("") || password.getText().toString().equals("")) {
+					SketchwareUtil.showMessage(getApplicationContext(), "All data Requered");
+				}
+				else {
+					auth.createUserWithEmailAndPassword(email.getText().toString().concat("_@ms.com"), password.getText().toString()).addOnCompleteListener(SplashActivity.this, _auth_create_user_listener);
+					SketchwareUtil.showMessage(getApplicationContext(), "Acoount created now login !");
+				}
+			}
+		});
+		
+		loginbtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				if (email.getText().toString().equals("") || password.getText().toString().equals("")) {
+					SketchwareUtil.showMessage(getApplicationContext(), "All data Requered");
+				}
+				else {
+					auth.signInWithEmailAndPassword(email.getText().toString().concat("_@ms.com"), password.getText().toString()).addOnCompleteListener(SplashActivity.this, _auth_sign_in_listener);
+					i.setClass(getApplicationContext(), GhjjActivity.class);
+					startActivity(i);
+				}
+			}
+		});
+		
+		auth_updateEmailListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_updatePasswordListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_emailVerificationSentListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_deleteUserListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_phoneAuthListener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> task) {
+				final boolean _success = task.isSuccessful();
+				final String _errorMessage = task.getException() != null ? task.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_updateProfileListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_googleSignInListener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> task) {
+				final boolean _success = task.isSuccessful();
+				final String _errorMessage = task.getException() != null ? task.getException().getMessage() : "";
+				
+			}
+		};
+		
+		_auth_create_user_listener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		_auth_sign_in_listener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		_auth_reset_password_listener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				
+			}
+		};
+	}
+	
+	private void initializeLogic() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) { Window w = getWindow();  w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS); };
+		linear15.setVisibility(View.GONE);
+		textview1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/hgh.ttf"), 0);
+		button1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/hgh.ttf"), 0);
+		button1.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)35, 0xFFFFFFFF));
+		loginbtn.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/hgh.ttf"), 0);
+		loginbtn.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)35, 0xFFFFFFFF));
+		createuser.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/hgh.ttf"), 0);
+		createuser.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)35, 0xFFFFFFFF));
+		email.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/hgh.ttf"), 0);
+		email.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)35, 0xFFFFFFFF));
+		password.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/hgh.ttf"), 0);
+		password.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)35, 0xFFFFFFFF));
+		linear15.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)35, (int)3, 0xFFFFFFFF, Color.TRANSPARENT));
+		if ((FirebaseAuth.getInstance().getCurrentUser() != null)) {
+			thjk = new TimerTask() {
+				@Override
+				public void run() {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							finish();
+						}
+					});
+				}
+			};
+			_timer.schedule(thjk, (int)(1600));
+		}
+		else {
+			thjk = new TimerTask() {
+				@Override
+				public void run() {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							splash.setVisibility(View.GONE);
+						}
+					});
+				}
+			};
+			_timer.schedule(thjk, (int)(1600));
+		}
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
